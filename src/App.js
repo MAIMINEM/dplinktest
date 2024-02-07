205edd43cc25044469e09be5eedc847f935396a0
MAIMINEM <104252557+MAIMINEM@users.noreply.github.com>
2024-02-07 08:33:32 +0800

    page update.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a className="App-link" href="https://dplinktest.netlify.app/">
-          Deep link to App
+          Deep link to Home Page
+        </a>
+        <h1></h1>
+        <a className="App-link" href="https://dplinktest.netlify.app/details">
+          Deep link to Detail Page
         </a>
       </header>
     </div>
