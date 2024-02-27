192cd85c69ac26057fe568d77fbac2b43dd63278
MAIMINEM <104252557+MAIMINEM@users.noreply.github.com>
2024-02-27 16:02:41 +0800

    adding space between elements.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a className="App-link" href="https://dplinktest.netlify.app/">
           Deep link to Home Page
         </a>
+
+        <div style={{ height: "50px" }} />
 
         <a className="App-link" href="https://dplinktest.netlify.app/details">
           Deep link to Details Page
